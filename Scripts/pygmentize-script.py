c323ca58b045a2520e447c7f4c1ab34f1cecf064
Jorge <jorgelopez.physics@gmail.com>
2013-06-16 23:24:03 +0200

    knkvfknfv

---
+++ Scripts/pygmentize-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\Jorge.Jorge-HP\VirtualDirectories\SphinxPr\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Pygments==1.6','console_scripts','pygmentize'
+__requires__ = 'Pygments==1.6'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Pygments==1.6', 'console_scripts', 'pygmentize')()
+)
---
+++ Scripts/sphinx-autogen-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\Jorge.Jorge-HP\VirtualDirectories\SphinxPr\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Sphinx==1.2b1','console_scripts','sphinx-autogen'
+__requires__ = 'Sphinx==1.2b1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Sphinx==1.2b1', 'console_scripts', 'sphinx-autogen')()
+)
